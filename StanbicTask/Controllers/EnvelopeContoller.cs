701bc6ef31a1b12e81f843d181339adcef19044e
EmmanuelImion <EmmanuelImion@MANNY>
2023-06-24 00:30:45 +0100

    Add project files.

---
+++ StanbicTask/Controllers/EnvelopeContoller.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StanbicTask.Dto;
+using System.Xml.Linq;
+
+namespace StanbicTask.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EnvelopeContoller : ControllerBase
+    {
+        
+        [HttpPost("post.{format}"), FormatFilter]
+        public IActionResult Post([FromBody] Envelope request)
+        {
+            
+            if (ModelState.IsValid)
+            {
+                return Ok(request);
+            }
+
+            return BadRequest("Pass in a valid xml payload!");
+        }
+
+        [HttpGet("get.{format}"),FormatFilter]
+
+        public IActionResult Get()
+        {
+            List<Envelope> envelopeList = new List<Envelope>();
+
+            // Create the first envelope object with sample data
+            Envelope envelope1 = new Envelope
+            {
+                Body = new Body
+                {
+                    NameEnquirySingleItem = new NameEnquirySingleItem
+                    {
+                        Request = new Request
+                        {
+                            NESingleRequest = new NESingleRequest
+                            {
+                                SessionId = "123456789",
+                                DestinationInstitutionCode = "ABC",
+                                ChannelCode = "XYZ",
+                                AccountNumber = "1234567890"
+                            }
+                        }
+                    }
+                }
+            };
+
+            envelopeList.Add(envelope1);
+
+            Envelope envelope2 = new Envelope
+            {
+                Body = new Body
+                {
+                    NameEnquirySingleItem = new NameEnquirySingleItem
+                    {
+                        Request = new Request
+                        {
+                            NESingleRequest = new NESingleRequest
+                            {
+                                SessionId = "987654321",
+                                DestinationInstitutionCode = "DEF",
+                                ChannelCode = "PQR",
+                                AccountNumber = "0987654321"
+                            }
+                        }
+                    }
+                }
+            };
+
+            envelopeList.Add(envelope2);
+
+            return Ok(envelopeList);
+
+        }
+    }
+
+    
+
+
+}
---
+++ StanbicTask/Dto/Envelope.cs
@@ -0,0 +1,43 @@
+﻿using System.Xml.Serialization;
+
+namespace StanbicTask.Dto
+{
+    [XmlRoot(ElementName = "Envelop")]
+    public class Envelope
+    {
+        [XmlElement(ElementName = "Body")]
+        public Body Body { get; set; }
+    }
+
+    public class Body
+    {
+        [XmlElement(ElementName = "name enquirysingleitem")]
+        public NameEnquirySingleItem NameEnquirySingleItem { get; set; }
+    }
+    public class NameEnquirySingleItem
+    {
+        [XmlElement(ElementName = "request")]
+        public Request Request { get; set; }
+    }
+
+
+    public class Request
+    {
+        [XmlElement(ElementName = "NESingleRequest")]
+        public NESingleRequest NESingleRequest { get; set; }
+    }
+    public class NESingleRequest
+    {
+        [XmlElement(ElementName = "SessionId")]
+        public string SessionId { get; set; }
+
+        [XmlElement(ElementName = "DestinationInstitutionCode")]
+        public string DestinationInstitutionCode { get; set; }
+
+        [XmlElement(ElementName = "ChannelCode")]
+        public string ChannelCode { get; set; }
+
+        [XmlElement(ElementName = "AccountNumber")]
+        public string AccountNumber { get; set; }
+    }
+}
